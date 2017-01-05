aed1a05fc71ccc80f4f900c3405f069a2e9571c6
haisheng huang <haisheng.huang@hotmail.com>
2017-01-06 00:15:22 +0800

    添加HSCustomView，并添加进度条的实现方法

---
+++ HSCustomView/HSCustomView/HSCustomView.swift
@@ -0,0 +1,31 @@
+//
+//  HSCustomView.swift
+//  HSCustomView
+//
+//  Created by haisheng huang on 2017/1/6.
+//  Copyright © 2017年 haisheng huang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HSCustomView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class func createProgress(at superView: UIView, frame: CGRect, deepColor: UIColor?, lightColor: UIColor?, backgroundColor: UIColor?, value: CGFloat, isCornerRadius: Bool, direction: GradientChangeDirection?, isAnimated: Bool?, duration: TimeInterval) -> Void {
+        
+        let progress: HSCustomProgress = HSCustomProgress.init(frame: frame, deepColor: deepColor, lightColor: lightColor, backgroundColor: backgroundColor, value: value, isCornerRadius: isCornerRadius, direction: direction, isAnimated: isAnimated, duration: duration)
+        
+        superView.addSubview(progress)
+
+    }
+
+}
+
